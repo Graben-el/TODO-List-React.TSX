9d8812aaae0b70520a33d00c0715f60caeade6dc
Gabriel Rodrigues <gabriel.int96@gmail.com>
2022-10-01 11:32:27 -0300

    first commit

---
+++ src/components/Footer.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import styles from './Footer.module.css'
+
+
+export const Footer = () => {
+    return (
+        <footer className={styles.footer}>
+            <p>
+                <span>React + TS Todo</span> @ 2022
+            </p>
+        </footer>
+    )
+};
+
